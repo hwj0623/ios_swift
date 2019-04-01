88933f2a44f3cd5d521313db67c6d416a679e822
hwj0623 <hwj0623@naver.com>
2019-04-01 17:26:30 +0900

    1-2. 2,3단 계산과 출력

--- step1/timestable.playground/Contents.swift
+++ step1/timestable.playground/Contents.swift
@@ -1,5 +1,21 @@
 import UIKit
 
-var str = "Hello, playground"
+
+//STEP 1-1
+var str = "1-1 Hello, playground \n"
 
 print (str)
+
+//STEP 1-2
+str = "==== 1-2 time tables ===="
+print (str)
+
+func multiply( input: Int, range : Int ) -> Void {
+    print("--- \(input) times table print --- \n")
+    for index in 1...range {
+        print("\(input) * \(index) = \(input * index) \n")
+    }
+}
+
+multiply(input: 2, range: 9)
+multiply(input: 3, range: 9)
