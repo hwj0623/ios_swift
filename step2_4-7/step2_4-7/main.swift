3c6e2b155b525dd49dee3206b742a88e712ce8bb
hwj0623 <hwj0623@naver.com>
2019-04-02 20:09:24 +0900

    2-5 completed

--- step2_4-7/step2_4-7/main.swift
+++ step2_4-7/step2_4-7/main.swift
@@ -59,8 +59,8 @@ extension String {
     }
 }
 
-// 2-2 FUNCTION : convert centimeter To Meter
-//  - 소숫점 처리를 위해 extension 활용. 첫째 자리에서 반올림
+////  - 소숫점 처리를 위해 extension 활용. 넷째 자리에서 반올림
+// 2-2 FUNCTION : 1 cm -> 0.01 m
 func convertCmToM (cmToM: String) -> String {
     let doubleValue : Double = NSString(string: cmToM as NSString).doubleValue
     let divisor: Double = 100
@@ -68,30 +68,67 @@ func convertCmToM (cmToM: String) -> String {
     print ("\(cmToM)cm -> \(result)m")
     return String(result)
 }
-
-// 2-2 FUNCTION : convert Meter To centimeter
-//  - 소숫점 처리를 위해 extension 활용. 4자리에서 반올림
-func convertMToCm (mToCm: String ) -> String {
-    let doubleValue : Double = NSString(string: mToCm as NSString).doubleValue
-    let times: Double = 100
-    let result = Double(doubleValue*times).rounded(toPlace: 4)
-    print ("\(mToCm)meter -> \(result)cm")
-    return String(result)
-}
-
-func convertInchToCm (inchToCm: String) -> String {
-    let doubleValue : Double = NSString(string: inchToCm as NSString).doubleValue
-    let times: Double = 2.54
-    let result = Double(doubleValue*times).rounded(toPlace: 4)
-    print ("\(inchToCm)inch -> \(result)cm")
-    return String(result)
-}
-
+// 2-2 FUNCTION : 1 cm -> 0.393701 inch
 func convertCmToInch (cmToInch: String) -> String {
     let doubleValue : Double = NSString(string: cmToInch as NSString).doubleValue
     let divisor: Double = 2.54
     let result = Double(doubleValue / divisor).rounded(toPlace: 4)
     print ("\(cmToInch)cm -> \(result)Inch")
+    return String(result)
+}
+
+// 2-2 FUNCTION : 1 m -> 100 cm
+func convertMToCm (mToCm: String ) -> String {
+    let doubleValue : Double = NSString(string: mToCm as NSString).doubleValue
+    let times: Double = 100
+    let result = Double(doubleValue * times).rounded(toPlace: 4)
+    print ("\(mToCm)meter -> \(result)cm")
+    return String(result)
+}
+
+// 2-2 FUNCTION : 1 inch -> 2.54 cm
+func convertInchToCm (inchToCm: String) -> String {
+    let doubleValue : Double = NSString(string: inchToCm as NSString).doubleValue
+    let times: Double = 2.54
+    let result = Double(doubleValue * times).rounded(toPlace: 4)
+    print ("\(inchToCm)inch -> \(result)cm")
+    return String(result)
+}
+
+
+// 2-5
+// yard --> 다른 단위로 환산
+// 1 yard -> 91.44 cm
+func convertYardToCm(yardToCm: String ) -> String{
+    let doubleValue : Double = NSString(string: yardToCm as NSString).doubleValue
+    let times: Double = 91.44
+    let result = Double(doubleValue * times).rounded(toPlace: 4)
+    print ("\(yardToCm) yard -> \(result) cm")
+    return String(result)
+}
+// 1 yard -> 36 inch
+func convertYardToInch (yardToInch: String )-> String{
+    let doubleValue : Double = NSString(string: yardToInch as NSString).doubleValue
+    let times: Double = 36
+    let result = Double(doubleValue * times).rounded(toPlace: 4)
+    print ("\(yardToInch) yard -> \(result) inch ")
+    return String(result)
+}
+// 다른 단위 -- > yard로 환산
+// 1 cm -> 0.0109361 yard
+func convertCmToYard(cmToYard: String ) -> String {
+    let doubleValue : Double = NSString(string: cmToYard as NSString).doubleValue
+    let divisor: Double = 91.44
+    let result = Double(doubleValue / divisor).rounded(toPlace: 4)
+    print ("\(cmToYard) cm -> \(result) yard")
+    return String(result)
+}
+// 1 inch -> 0.0277778 yard
+func convertInchToYard(inchToYard: String )-> String{
+    let doubleValue : Double = NSString(string: inchToYard as NSString).doubleValue
+    let times: Double = 36
+    let result = Double(doubleValue * times).rounded(toPlace: 4)
+    print ("\(inchToYard) inch -> \(result) yard ")
     return String(result)
 }
 
@@ -114,6 +151,9 @@ func unitConverter (number: String, input: String) -> Void {
         _ =  convertMToCm(mToCm: number)
     case "inch" :
         _ = convertInchToCm(inchToCm: number)
+    case "yard" :
+        let bypass = convertYardToCm(yardToCm: number)
+        _ = convertCmToM(cmToM: bypass)
     default :
         print("지원하지 않는 단위입니다.")
     }
@@ -127,6 +167,8 @@ func unitConverter(number: String, from: String, to : String) -> Void {
             fromMToWhat(number: number, to: to)
         case "inch":
             fromInchToWhat(number: number, to: to)
+        case "yard":
+            fromYardToWhat(number: number, to: to)
         default :
             print("지원하지 않는 단위입니다.")
     }
@@ -138,6 +180,8 @@ func fromCmToWhat (number: String, to: String) -> Void{
             _ = convertCmToM(cmToM: number)
     case "inch":
             _ = convertCmToInch(cmToInch: number)
+    case "yard":
+            _ = convertCmToYard(cmToYard: number)
     default:
         print("지원하지 않는 단위입니다.")
     }
@@ -150,6 +194,9 @@ func fromMToWhat (number: String, to: String )-> Void {
     case "inch":
         let bypass = convertMToCm(mToCm: number)
         _ = convertCmToInch(cmToInch: bypass)
+    case "yard":
+        let bypass = convertMToCm(mToCm: number)
+        _ = convertCmToYard(cmToYard: bypass)
     default:
         print("지원하지 않는 단위입니다.")
     }
@@ -162,6 +209,24 @@ func fromInchToWhat (number: String, to: String) -> Void {
     case "m":
         let bypass = convertInchToCm(inchToCm: number)
         _ = convertCmToM(cmToM: bypass)
+    case "yard":
+        _ = convertInchToYard(inchToYard: number)
+    default:
+        print("지원하지 않는 단위입니다.")
+    }
+}
+
+// 2-5
+// FUNCTION : yard 에서 변환할 수치와 단위를 나타내는 서브루틴 함수
+func fromYardToWhat(number: String, to: String) -> Void {
+    switch to {
+    case "cm":
+        _ = convertYardToCm(yardToCm: number)
+    case "m":
+        let bypass = convertYardToCm(yardToCm: number)
+        _ = convertCmToM(cmToM: bypass)
+    case "inch":
+        _ = convertYardToInch(yardToInch: number)
     default:
         print("지원하지 않는 단위입니다.")
     }
@@ -172,20 +237,28 @@ func fromInchToWhat (number: String, to: String) -> Void {
 while true {
     
     let str = readLine()
+    // 2-5 종료조건인 경우 탈출
+    if(str == "quit" || str == "q"){
+        break
+    }
     
+    // unit converter process 시작
     if let test = str {
+        
         let firstIdx = test.firstIndex(of: " ") ?? test.endIndex
         let tmpStr = String(test[..<firstIdx])
         let strArr =  tmpStr.unitSeperate()
-//        print ("parse number and unit : > \(strArr) ")
-//        print ("start : \(strArr.startIndex) , end : \(strArr.endIndex)")
+
         let unitIndex = strArr.endIndex-1
         let numberIndex = strArr.startIndex
-        
+      
         //규격에 안맞으면 처음부터 다시
         if strArr[unitIndex].lowercased() != "cm"
             && strArr[unitIndex].lowercased() != "m"
-            && strArr[unitIndex].lowercased() != "inch" {
+            && strArr[unitIndex].lowercased() != "inch"
+            && strArr[unitIndex].lowercased() != "yard"
+        {
+            print("지원하지 않는 단위입니다.")
             continue
         }
         
@@ -194,12 +267,10 @@ while true {
             let secondStr = test.split(separator: " ",  omittingEmptySubsequences: true)[1]
 //            print ("to unit > \(secondStr)")
             unitConverter(number: strArr[numberIndex], from: strArr[unitIndex], to: String(secondStr))
-            break
         }
          //1 input element
         else{
             unitConverter(number: strArr[numberIndex], input: strArr[unitIndex])
-            break
         }
     }
 }
