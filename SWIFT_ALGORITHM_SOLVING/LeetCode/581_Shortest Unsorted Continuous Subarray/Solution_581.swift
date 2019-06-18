f2f9b4b343cdef7c00f9fed89a19081f246a5ade
hwj0623 <hwj0623@naver.com>
2019-06-18 21:18:32 +0900

    581번 문제 풀이중..

---
+++ SWIFT_ALGORITHM_SOLVING/LeetCode/581_Shortest Unsorted Continuous Subarray/Solution_581.swift
@@ -0,0 +1,19 @@
+//
+//  Solution_581.swift
+//  SWIFT_ALGORITHM_SOLVING
+//
+//  Created by hw on 18/06/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import Foundation
+
+class Solution_581 {
+    func findUnsortedSubarray(_ nums: [Int]) -> Int {
+        
+        for (index, value) in nums.enumerated() {
+            
+            
+        }
+    }
+}
