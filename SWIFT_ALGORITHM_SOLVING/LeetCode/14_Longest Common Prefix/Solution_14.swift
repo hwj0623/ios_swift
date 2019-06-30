e592303254b204127765be678f00ffea904a1b08
hwj0623 <hwj0623@naver.com>
2019-06-30 23:56:18 +0900

    [6/30] 14번 AC

--- SWIFT_ALGORITHM_SOLVING/LeetCode/14_Longest Common Prefix/Solution_14.swift
+++ SWIFT_ALGORITHM_SOLVING/LeetCode/14_Longest Common Prefix/Solution_14.swift
@@ -12,22 +12,56 @@ import Foundation
 
 
 class Solution_Leetcode_14{
+//    func longestCommonPrefix(_ strs: [String]) -> String {
+//        var result = ""
+//
+//        guard let firstWord = strs.first, let shortestWordCount = strs.map({$0.count}).min(), shortestWordCount > 0 else {
+//            return result
+//
+//        }
+//
+//        for (index, char) in firstWord.enumerated() {
+//            let matchedWords = strs.filter({$0[$0.index($0.startIndex, offsetBy: index)] == char})
+//            if matchedWords.count == strs.count {
+//                result.append(char)
+//            } else {
+//                return result
+//            }
+//            if index == shortestWordCount - 1 { return result }
+//        }
+//
+//        return result
+//    }
+    
     func longestCommonPrefix(_ strs: [String]) -> String {
+        let allCount = strs.count
+        if strs.count == 0 {
+            return ""
+        }
+        if strs.count == 1{
+            return strs[0]
+        }
         var minLen = strs[0].count
         for i in 0..<strs.endIndex {
             if minLen > strs[i].count {
                 minLen = strs[i].count
             }
         }
-        var allCount = strs.count
         var result = ""
+        var isEnd = false
         for j in 0..<minLen {
             let cur = strs[0][j]
             var count = 0
             for i in 0..<strs.endIndex {
                 if strs[i][j] == cur {
                     count += 1
+                }else{
+                    isEnd = true
+                    break
                 }
+            }
+            if isEnd {
+                break
             }
             if count == allCount {
                 result = result + strs[0][j]
@@ -42,6 +76,13 @@ class Solution_Leetcode_14{
         print(result)
         let test2 = ["dog","racecar","car"]
         result = longestCommonPrefix(test2)
+        let test3 = [String]()
+        result = longestCommonPrefix(test3)
+        print(result)
+
+        let test4 = ["aca","cba"]
+        result = longestCommonPrefix(test4)
+        
         print(result)
     }
 }
