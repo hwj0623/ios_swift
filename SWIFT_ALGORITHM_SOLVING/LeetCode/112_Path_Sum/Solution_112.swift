9a9a3ff0bf4739e6d7d47c17e7badc5c863423af
hwj0623 <hwj0623@naver.com>
2019-07-15 21:20:32 +0900

    112_Path_Sum start solving

---
+++ SWIFT_ALGORITHM_SOLVING/LeetCode/112_Path_Sum/Solution_112.swift
@@ -0,0 +1,30 @@
+//
+//  Solution_112.swift
+//  SWIFT_ALGORITHM_SOLVING
+//
+//  Created by hw on 15/07/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import Foundation
+
+class Solution_Leetcode_112{
+    class TreeNode{
+        public var val: Int
+        public var left: TreeNode?
+        public var right: TreeNode?
+        public init(_ val: Int) {
+            self.val = val
+            self.left = nil
+            self.right = nil
+        }
+    }
+    
+    func hasPathSum(_ root: TreeNode?, _ sum: Int) -> Bool {
+        
+    }
+    
+    func start(){
+        
+    }
+}
