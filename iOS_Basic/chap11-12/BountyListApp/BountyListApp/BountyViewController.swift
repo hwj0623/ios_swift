30eea582891d44b23e2550c1c037e00a5d93357f
hwj0623 <hwj0623@naver.com>
2019-07-21 17:22:38 +0900

    UITableView -> UICollectionView 로 현상금 리스트 표시 변경

--- iOS_Basic/chap11-12/BountyListApp/BountyListApp/BountyViewController.swift
+++ iOS_Basic/chap11-12/BountyListApp/BountyListApp/BountyViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+class BountyViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout  {
+    
+    
     @IBOutlet weak var bountyTableView: UITableView!
     var bountyInfoList = [
                             BountyInfo(name: "brook", bounty: 3300000),
@@ -21,28 +23,59 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
                             BountyInfo(name: "zoro", bounty: 120000000),
 
                          ]
-    /// 데이터 몇개를 보여줄 것인가?
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    /*
+     * collectionView
+     */
+    /// 몇 개를 보여줄 것인가?
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return bountyInfoList.count
     }
-
-    /// 셀은 어떻게 표현할 것인가? - customizing한 UITableViewCell인 ListCell을 사용한다.
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        /// 재사용가능한 셀을 사용한다.
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
-            return UITableViewCell()
+    
+    ///셀은 어떻게 표현할 것인가? - 한줄표기가 아니므로 몇 번째 아이템인지
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else{
+            return UICollectionViewCell()
         }
-        /// img, name, bounty
-        let bountyInfo = bountyInfoList[indexPath.row]
-        cell.updateUI(bountyInfo)
+        let info = bountyInfoList[indexPath.item]
+        cell.updateUI(info)
         return cell
     }
-
     /// 셀 클릭시 어떻게 할 것인지
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        /// 세그웨이의 id를 지정하여 sender로 데이터를 넘긴다.
-        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        /// indexPath의 item을 지정하여 sender로 데이터를 넘긴다.
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
     }
+    
+    /// cell size 를 기기에 맞게 - UICollectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = (collectionView.bounds.width-10)/2
+        let height: CGFloat = width * 10/7 + 65
+        return CGSize.init(width: width, height: height)
+    }
+    /*
+     * tableView
+     */
+//    /// 데이터 몇개를 보여줄 것인가?
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return bountyInfoList.count
+//    }
+//
+//    /// 셀은 어떻게 표현할 것인가? - customizing한 UITableViewCell인 ListCell을 사용한다.
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        /// 재사용가능한 셀을 사용한다.
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+//            return UITableViewCell()
+//        }
+//        /// img, name, bounty
+//        let bountyInfo = bountyInfoList[indexPath.row]
+//        cell.updateUI(bountyInfo)
+//        return cell
+//    }
+//    /// 셀 클릭시 어떻게 할 것인지
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        /// 세그웨이의 id를 지정하여 sender로 데이터를 넘긴다.
+//        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+//    }
     
     /// segueway 실행시 목적지 vc에 전송할 데이터를 처리한다.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -60,7 +93,7 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     }
 }
 
-class ListCell: UITableViewCell {
+class GridCell: UICollectionViewCell {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
@@ -71,3 +104,15 @@ class ListCell: UITableViewCell {
         bountyLabel.text = "\(bountyInfo.bounty)"
     }
 }
+
+//class ListCell: UITableViewCell {
+//    @IBOutlet weak var imgView: UIImageView!
+//    @IBOutlet weak var nameLabel: UILabel!
+//    @IBOutlet weak var bountyLabel: UILabel!
+//
+//    func updateUI(_ bountyInfo: BountyInfo){
+//        imgView.image = bountyInfo.image
+//        nameLabel.text = bountyInfo.name
+//        bountyLabel.text = "\(bountyInfo.bounty)"
+//    }
+//}
---
+++ iOS_Basic/chap13/UICollectionView.md
@@ -0,0 +1,21 @@
+## UICollectionView
+
+- UITableView는 리스트형태로 1열로 보여주는 형태로 구성되어있음.
+
+- 화면을 그리드 형태로 다양하게 레이아웃하여 보여줄 수 있음
+- 하나 이상의 컬럼
+- 데이터의 나열을 수평/수직방향으로 줄 것인지 지정 가능
+- 직접 커스터마이징 가능
+
+
+
+## UICollectionViewLayout
+
+- 줄 정렬 등 CollectionView를 꾸미기 위한 레이아웃 객체
+
+- **`UICollectionViewFlowLayout`** 은 iOS에서 기본으로 제공하는 레이아웃 
+- Datasource & Delegate 패턴을 사용
+
+
+
+TableView나 CollectionView도 cell에 대한 reuse를 위해 cell에 대해 Attribute Inspector에서 identifier를 지정하는 것이 좋다.
